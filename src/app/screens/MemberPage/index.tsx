2b9c60f8dfb9e893449c72d4fdc4327ff9347d6e
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-11-28 20:34:24 +0900

    feat:publishing of my page in member page

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,6 +1,23 @@
-import React from "react";
 import { Container } from "@mui/material";
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { VisitOtherPage } from "./VisitOtherPage";
+import { VisitMyPage } from "./VisitMyPage";
+import "../../../css/my_page.css";
 
 export function MemberPage() {
-  return <Container>MemberPage</Container>;
+  let member = useRouteMatch();
+  console.log(member);
+  return (
+    <div className="restaurant_page">
+      <Switch>
+        <Route path={`${member.path}/other`}>
+          <VisitOtherPage />
+        </Route>
+        <Route path={`${member.path}`}>
+          <VisitMyPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
