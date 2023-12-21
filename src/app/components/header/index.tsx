d2660ffc04e3ce82b2d21f33174d452cb250402f
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-12-21 18:46:02 +0900

    feat: practise useEffect hook on 3 lifecycle method feautures

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -6,12 +6,17 @@ import {
   IconButton,
   Badge,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export function NavbarHome(props: any) {
   // INITIALIZATIONS
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState(true);
+
+  useEffect(() => {
+    setCount(count + 1);
+  }, [value]);
 
   return (
     <div className="format home_navbar">
@@ -113,7 +118,7 @@ export function NavbarHome(props: any) {
                   background: "#1976d2",
                   color: "#fff",
                 }}
-                onClick={() => setCount(count + 1)}
+                onClick={() => setValue(!value)}
               >
                 RO'YHATDAN O'TISH
               </Button>
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,5 +1,5 @@
 import { Container } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { Statistics } from "./statistics";
 import { TopRestaurants } from "./topRestaurants";
 import { BestRestaurants } from "./bestRestaurants";
@@ -10,6 +10,14 @@ import { Recommendations } from "./recommendation";
 import "../../../css/home.css";
 
 export function HomePage() {
+  useEffect(() => {
+    console.log("componentDidMount => Data Fetch");
+
+    return () => {
+      console.log("componentWillUnmount process");
+    };
+  }, []);
+
   return (
     <div className="homepage">
       <Statistics />
