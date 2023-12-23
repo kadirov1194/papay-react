7db03a5f0f97b7dc6e63400c3cac22d4be7904a7
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-12-23 17:45:05 +0900

    fix: usage of slice and selectors on homePage screen component

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/HomePage/slice";
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare().concat(reduxLogger),
   reducer: {
     homePage: HomePageReducer,
   },
