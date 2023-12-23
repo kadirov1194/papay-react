a8517dc721cabdb2f5ca971a79c8b68b91006f02
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-12-23 16:47:25 +0900

    feat: homePage screen slice & selector logic creation

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -6,18 +6,42 @@ import { BestRestaurants } from "./bestRestaurants";
 import { BestDishes } from "./bestDishes";
 import { Advertisements } from "./advertisements";
 import { Events } from "./events";
-import { Recommendations } from "./recommendation";
 import "../../../css/home.css";
+import { Recommendations } from "./recommendations";
+
+//REDUX
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setTopRestaurants } from "./slice";
+import { retrieveTopRestaurants } from "./selector";
+import { Restaurant } from "../../../types/user";
+
+// REDUX SLICE
+const actionDispatch = (dispach: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispach(setTopRestaurants(data)),
+});
+// REDUX SELECTOR
+const topRestaurantRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
 
 export function HomePage() {
+  //INITIALIZATION
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantRetriever);
+  //selector: store => data
+
   useEffect(() => {
-    console.log("componentDidMount => Data Fetch");
+    //backend data request => data
 
-    return () => {
-      console.log("componentWillUnmount process");
-    };
+    setTopRestaurants([]);
+
+    //slice: data => store
   }, []);
-
   return (
     <div className="homepage">
       <Statistics />
