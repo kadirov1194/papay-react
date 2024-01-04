5d40804ccf9247137cbf468d54e29ca9326d08e1
kadirov1194 <shoxrux.qodirov@gmail.com>
2024-01-04 19:01:58 +0900

    feat: CommunityPage redux store configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,24 @@
-import { configureStore, ThunkAction, Action, getDefaultMiddleware } from '@reduxjs/toolkit';
-import HomePageReducer from './screens/HomePage/slice';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+import HomePageReducer from "./screens/HomePage/slice";
 import reduxLogger from "redux-logger";
-import RestaurantPageReducer from './screens/RestaurantPage/slice';
-import OrdersPageReducer from './screens/OrdersPage/slice';
+import RestaurantPageReducer from "./screens/RestaurantPage/slice";
+import OrdersPageReducer from "./screens/OrdersPage/slice";
+import CommunityPageReducer from "./screens/CommunityPage/slice";
 
 export const store = configureStore({
-  middleware: (getDefaultMiddleware) => 
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(reduxLogger),
-  
+
   reducer: {
     homePage: HomePageReducer,
     restaurantPage: RestaurantPageReducer,
     ordersPage: OrdersPageReducer,
+    communityPage: CommunityPageReducer,
   },
 });
 
--- src/types/screen.ts
+++ src/types/screen.ts
@@ -3,36 +3,41 @@ import { Order } from "./order";
 import { Product } from "./product";
 import { Restaurant } from "./user";
 
-
 /** REACT APP STATE */
 export interface AppRootState {
-    homePage: HomePageState;
-    restaurantPage: RestaurantPageState;
-    ordersPage: OrdersPageState;
+  homePage: HomePageState;
+  restaurantPage: RestaurantPageState;
+  ordersPage: OrdersPageState;
+  communityPage: CommunityPageState;
 }
 
 /**  HOMEPAGE  */
-export interface HomePageState{
-    topRestaturants: Restaurant[];
-    bestRestaurants: Restaurant[];
-    trendProducts: Product[];
-    bestBoArticles: BoArticle[];
-    trendBoArticles: BoArticle[];
-    newsBoArticles: BoArticle[];
+export interface HomePageState {
+  topRestaturants: Restaurant[];
+  bestRestaurants: Restaurant[];
+  trendProducts: Product[];
+  bestBoArticles: BoArticle[];
+  trendBoArticles: BoArticle[];
+  newsBoArticles: BoArticle[];
 }
 
 /**   RESTAURANT PAGE  */
 export interface RestaurantPageState {
-    targetRestaurants: Restaurant[];
-    randomRestaurants: Restaurant[];
-    chosenRestaurant: Restaurant | null;
-    targetProducts: Product[];
-    chosenProduct: Product | null;
+  targetRestaurants: Restaurant[];
+  randomRestaurants: Restaurant[];
+  chosenRestaurant: Restaurant | null;
+  targetProducts: Product[];
+  chosenProduct: Product | null;
 }
 
 /** Orders */
 export interface OrdersPageState {
-    pausedOrders: Order[],
-    processOrders: Order[],
-    finishedOrders: Order[]
+  pausedOrders: Order[];
+  processOrders: Order[];
+  finishedOrders: Order[];
+}
+
+/** Community Page */
+export interface CommunityPageState {
+  targetBoArticles: BoArticle[];
 }
